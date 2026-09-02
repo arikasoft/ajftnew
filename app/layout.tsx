234963738bf9e763ab83653430702fd9d2847a2a
arikasoft <support@arikasoft.com>
2026-09-02 17:04:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import AnalyticsTracker from "@/components/analytics/AnalyticsTracker";
 
 export const metadata: Metadata = {
   title: "Anand Jivan Foundation Trust",
@@ -19,8 +20,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Consent Mode - MUST load before GTM */}
+        <Script id="consent-mode-default" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('consent', 'default', {
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+              ad_user_data: 'denied',
+              ad_personalization: 'denied',
+              wait_for_update: 500
+            });
+          `}
+        </Script>
+
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="beforeInteractive">
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){
               w[l]=w[l]||[];
@@ -31,19 +48,31 @@ export default function RootLayout({
 
               var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),
-                  dl=l!='dataLayer' ? '&l='+l : '';
+                  dl=l!='dataLayer'?'&l='+l:'';
 
               j.async=true;
               j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
 
               f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MJTS5M8N');
+            })(
+              window,
+              document,
+              'script',
+              'dataLayer',
+              'GTM-MJTS5M8N'
+            );
           `}
         </Script>
+
+        {/* CookieYes */}
+        <Script
+          id="cookieyes"
+          src="https://cdn-cookieyes.com/client_data/5cfe4f28d3b2e213a2c75d46/script.js"
+          strategy="beforeInteractive"
+        />
       </head>
 
       <body className="bg-white text-gray-900 antialiased">
-
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -58,6 +87,8 @@ export default function RootLayout({
         </noscript>
 
         <Header />
+
+        <AnalyticsTracker />
 
         {children}
 
